ffa76115d19e29be1daa6df909cb8b05e830c0f0
SebasSalazar <sebajuan3794@gmail.com>
2023-07-24 22:22:45 -0500

    nnh

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//Booleano="ADSO";
+//console.log(typeof Booleano)
+
+/*const nom="sebastian"
+const ape='salazar'
+alert("mi nombre es "+nom+" y mi apellido es "+ape)*/
+
+//operadores aritmeticos//
+
+/*const num1=5
+const num2=2.5
+const suma= num1+num2
+parseInt (suma)
+alert("La suma es "+suma)*/
+
+/*const num1=5
+const num2=2.5
+const resta= num1-num2
+parseInt (resta)
+alert("La resta es "+resta)*/
+
+/*const num1=5
+const num2=5
+const multi= num1*num2
+parseInt (multi)
+alert("La multiplicacion es "+multi)*/
+
+/*const num1=5
+const num2=3
+const divi= num1/num2
+parseInt (divi)
+alert("La division es "+divi)*/
+
+/*let nom=prompt("Escriva el nombre")
+alert("mi nombre es"+nom)*/
+
+let nom=prompt("Escrivir los nombres")
+let ape=prompt("Escrivir los apellidos")
+let edad=prompt("Escrivir la edad")
+let peso=prompt("Escrivir el peso")
+let ciu=prompt("Escrivir la ciudad de recidencia")
+alert("mi nombre es "+nom+" "+ape+ " tengo " +edad+ " años, peso "+peso+ "kg y vivo en "+ciu)
+
+
+
+
